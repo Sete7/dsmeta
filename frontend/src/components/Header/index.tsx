fbc414d06e8cd3e76bfa0337519f1c1c8bfcf2ef
Junio Santos <junio.santos.ps@gmail.com>
2022-07-12 02:13:27 -0300

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,19 +1,19 @@
-import logo from '../../assets/img/logo.svg'
-import './style.css'
+import logo from '../../assets/img/logo.svg';
+import './style.css';
 
 function Header(){
     return(
         <header>
         <div className="dsmeta-logo-container">
             <img src={logo} alt="DSMeta" />
-            <h1>DSMeta</h1>
+            <h1>DSMeta React and spring</h1>
             <p>
               Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+              <a href="https://www.linkedin.com/in/junio-santos-985ba2103/">@junio_ps</a>
             </p>
         </div>
     </header>
     )
 }
 
-export default Header
+export default Header;
